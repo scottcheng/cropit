8fccb9d93a5f58179e07254610682d093fbde42d
Scott Cheng <me@scottcheng.com>
2015-07-08 11:50:52 +0200

    Compile options from options.js

---
+++ src/generate_options_doc.js
@@ -0,0 +1,31 @@
+import fs from 'fs';
+
+import options from './options';
+
+let output = '';
+
+['elements', 'values', 'callbacks'].forEach((k) => {
+  output += `//- ${k}\n`;
+
+  options[k].forEach((o) => {
+    output += '.option\n';
+    output += `  .name ${o.name}\n`;
+    output += `  .type ${o.type}\n`;
+    output += `  .description\n    :markdown\n      ${o.description}\n`;
+    if (o.type !== 'function') {
+      if (typeof o.default === 'string') {
+        output += `  .default: code '${o.default}'\n`;
+      }
+      else if (Array.isArray(o.default)) {
+        output += `  .default: code [${o.default}]\n`;
+      }
+      else {
+        output += `  .default: code ${o.default}\n`;
+      }
+    }
+  });
+
+  output += '\n';
+});
+
+fs.writeFileSync(__dirname + '/views/_options.jade', output);
